1e39637b4578dc33c87d59044b5e61682c5572d8
Krupali Lalakiya <krupali.lalakiya@gmail.com>
2025-05-04 14:22:55 +0530

    fix style

--- src/components/SolidButton.jsx
+++ src/components/SolidButton.jsx
@@ -3,7 +3,7 @@ import {  Button} from 'antd';
 const SolidButton = ({ children, onClick }) => {
   return (
     <div>
-        <Button  type="primary" variant='solid' htmlType="submit" className="signup-btn" onClick={onClick} style={{margin:"10px 0px"}}>
+        <Button  type="primary" variant='solid' htmlType="submit" className="solid-btn" onClick={onClick} style={{margin:"10px 0px"}}>
           {/* Sign Up */}
           {children}
         </Button>
